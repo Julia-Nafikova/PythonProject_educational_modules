33106f7e472c84a9ae5849bcb2b136a2e4faa9f8
Julia <nafikova91@yandex.ru>
2025-06-02 18:10:00 +0300

    test 1

--- config/urls.py
+++ config/urls.py
@@ -12,11 +12,9 @@ schema_view = get_schema_view(
       description="Test description",
       terms_of_service="https://www.google.com/policies/terms/",
       contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
-   ),
+      license=openapi.License(name="BSD License")),
    public=True,
-   permission_classes=(permissions.AllowAny,),
-)
+   permission_classes=[permissions.AllowAny])
 
 
 urlpatterns = [
